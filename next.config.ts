a22a6feaca5f73e08232809e41b327b5a6bd9898
idrissou <idrissoufaysal93@gmail.com>
2026-06-10 11:02:24 +0000

    u

--- next.config.ts
+++ next.config.ts
@@ -33,12 +33,15 @@ const securityHeaders = [
 
 const nextConfig: NextConfig = {
   async headers() {
+    if (process.env.NODE_ENV !== 'production') {
+      return [];
+    }
     return [
       {
         source: '/(.*)',
         headers: securityHeaders,
       },
-    ]
+    ];
   },
   images: {
     remotePatterns: [
